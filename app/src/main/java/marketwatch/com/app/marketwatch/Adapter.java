cc7779f8fe794850dfa34cebf6078c6577fdae6a
pratikb <pratik822@gmail.com>
2018-03-07 17:53:20 +0530

    layout changes and adapter added

--- app/src/main/java/marketwatch/com/app/marketwatch/Adapter.java
+++ app/src/main/java/marketwatch/com/app/marketwatch/Adapter.java
@@ -1,10 +1,12 @@
 package marketwatch.com.app.marketwatch;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -14,36 +16,87 @@ import java.util.List;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.Viewholder> {
     Context myctx;
-    List<NewsData>mydata;
+    List<NewsData> mydata;
 
-    public Adapter(Context ctx, List<NewsData>data){
-        this.myctx=ctx;
-        this.mydata=data;
+    public Adapter(Context ctx, List<NewsData> data) {
+        this.myctx = ctx;
+        this.mydata = data;
     }
 
     @Override
     public Viewholder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list,null);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_lists, null);
         return new Viewholder(view);
     }
 
     @Override
     public void onBindViewHolder(Viewholder holder, int position) {
 
+
+            holder.tv_stockname.setText(mydata.get(position).getStorename());
+            holder.tv_posteddate.setText(mydata.get(position).getCreate_date());
+            holder.tv_buyprice.setText("BUY @" + mydata.get(position).getBuyprice());
+            if (mydata.get(position).getStoploss().isEmpty()) {
+                holder.tv_stoploss.setText("STOPLOSS " + "-");
+            } else {
+                holder.tv_stoploss.setText("STOPLOSS " + mydata.get(position).getStoploss());
+            }
+
+            if(mydata.get(position).getTarget1().isEmpty()){
+                holder.tv_target_one.setText("-");
+            }else{
+                holder.tv_target_one.setText("@" + mydata.get(position).getTarget1());
+            }
+
+        if(mydata.get(position).getTarget2().isEmpty()){
+            holder.tv_target_two.setText("-");
+        }else{
+            holder.tv_target_two.setText("@" + mydata.get(position).getTarget2());
+        }
+
+        if(mydata.get(position).getTarget3().isEmpty()){
+            holder.tv_target_three.setText("-");
+        }else{
+            holder.tv_target_three.setText("@" + mydata.get(position).getTarget3());
+        }
+
+
+            if (mydata.get(position).getType().equalsIgnoreCase("buy")) {
+                holder.tv_type.setText("BUY CALL");
+            }
+            holder.tv_duration.setText(mydata.get(position).getDuration());
+            if (mydata.get(position).getDuration().contains("Hit")) {
+                holder.tv_duration.setBackgroundColor(ContextCompat.getColor(myctx, android.R.color.holo_red_dark));
+            } else {
+                holder.tv_duration.setBackgroundColor(ContextCompat.getColor(myctx, android.R.color.holo_green_dark));
+            }
+
+
+
     }
 
     @Override
     public int getItemCount() {
-        return 8;
+        return mydata.size();
     }
 
-    class  Viewholder extends RecyclerView.ViewHolder{
+    class Viewholder extends RecyclerView.ViewHolder {
+        TextView tv_stockname, tv_posteddate, tv_buyprice, tv_stoploss, tv_target_one, tv_target_two, tv_target_three, tv_type, tv_duration;
 
         public Viewholder(View itemView) {
             super(itemView);
+            tv_stockname = (TextView) itemView.findViewById(R.id.tv_stockname);
+            tv_posteddate = (TextView) itemView.findViewById(R.id.tv_posteddate);
+            tv_buyprice = (TextView) itemView.findViewById(R.id.tv_buyprice);
+            tv_stoploss = (TextView) itemView.findViewById(R.id.tv_stoploss);
+            tv_target_one = (TextView) itemView.findViewById(R.id.tv_target_one);
+            tv_target_two = (TextView) itemView.findViewById(R.id.tv_target_two);
+            tv_target_three = (TextView) itemView.findViewById(R.id.tv_target_three);
+            tv_type = (TextView) itemView.findViewById(R.id.tv_call);
+            tv_duration = (TextView) itemView.findViewById(R.id.tv_status);
+
         }
     }
-
 
 
 }
--- app/src/main/java/marketwatch/com/app/marketwatch/MainActivity.java
+++ app/src/main/java/marketwatch/com/app/marketwatch/MainActivity.java
@@ -2,6 +2,7 @@ package marketwatch.com.app.marketwatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -18,6 +19,7 @@ import retrofit.Retrofit;
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recycleview;
     List<NewsData>datalist=new ArrayList<>();
+    List<NewsData>finaldatalist=new ArrayList<>();
     LinearLayoutManager mLayoutManager;
 
     @Override
@@ -25,11 +27,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recycleview=(RecyclerView)findViewById(R.id.recycleview);
-        recycleview.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
         recycleview.setLayoutManager(mLayoutManager);
-        Adapter adapter=new Adapter(MainActivity.this,datalist);
-        recycleview.setAdapter(adapter);
+        recycleview.setItemAnimator(new DefaultItemAnimator());
+
        RetrofitService service=RetrofitClient.getApiService();
 
 
@@ -37,11 +38,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Response<List<NewsData>> response, Retrofit retrofit) {
                 Log.d("responce",new Gson().toJson(response));
+                 datalist=response.body();
+
+                 for (int i=0;i<datalist.size();i++){
+                     if(!datalist.get(i).getBuyprice().equalsIgnoreCase("info")&& !datalist.get(i).getTarget1().equalsIgnoreCase("info")){
+                         finaldatalist.add(datalist.get(i));
+                     }
+                 }
+
+                if(finaldatalist.size()>0){
+                    Adapter adapter=new Adapter(MainActivity.this,finaldatalist);
+                    recycleview.setAdapter(adapter);
+                }
             }
 
             @Override
             public void onFailure(Throwable t) {
-
+                t.printStackTrace();
             }
         });
     }
