df31b30cc26c50c8cf2d1cd5b222c1f4ad325f2e
Pratik <pratik822@gmail.com>
2018-03-11 21:25:25 +0530

    share

--- app/src/main/java/marketwatch/com/app/marketwatch/Adapter.java
+++ app/src/main/java/marketwatch/com/app/marketwatch/Adapter.java
@@ -1,11 +1,14 @@
 package marketwatch.com.app.marketwatch;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -85,8 +88,9 @@ public class Adapter extends RecyclerView.Adapter<Adapter.Viewholder> {
 
     class Viewholder extends RecyclerView.ViewHolder {
         TextView tv_stockname, tv_posteddate, tv_buyprice, tv_stoploss, tv_target_one, tv_target_two, tv_target_three, tv_type, tv_duration;
+        private ImageView iv_share;
 
-        public Viewholder(View itemView) {
+        public Viewholder(final View itemView) {
             super(itemView);
             tv_stockname = (TextView) itemView.findViewById(R.id.tv_stockname);
             tv_posteddate = (TextView) itemView.findViewById(R.id.tv_posteddate);
@@ -97,8 +101,42 @@ public class Adapter extends RecyclerView.Adapter<Adapter.Viewholder> {
             tv_target_three = (TextView) itemView.findViewById(R.id.tv_target_three);
             tv_type = (TextView) itemView.findViewById(R.id.tv_call);
             tv_duration = (TextView) itemView.findViewById(R.id.tv_status);
+            iv_share = (ImageView) itemView.findViewById(R.id.iv_share);
+            iv_share.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String msg = mydata.get(getAdapterPosition()).getStorename();
+                    if (!mydata.get(getAdapterPosition()).getBuyprice().isEmpty()) {
+                        msg += " Buy@" + mydata.get(getAdapterPosition()).getBuyprice() + "";
+                    }
+                    if (!mydata.get(getAdapterPosition()).getStoploss().isEmpty() || mydata.get(getAdapterPosition()).getStoploss().isEmpty()) {
+                        msg += " stoploss@" + mydata.get(getAdapterPosition()).getStoploss() + "";
+                    }
+                    if (!mydata.get(getAdapterPosition()).getTarget1().isEmpty() || !mydata.get(getAdapterPosition()).getTarget1().contains("-")) {
+                        msg += " target1@" + mydata.get(getAdapterPosition()).getTarget1() + "";
+                    }
+                    if (!mydata.get(getAdapterPosition()).getTarget2().isEmpty() || !mydata.get(getAdapterPosition()).getTarget2().contains("-")) {
+                        msg += " target2@" + mydata.get(getAdapterPosition()).getTarget2() + "";
+                    }
+                    if (!mydata.get(getAdapterPosition()).getTarget3().isEmpty() || !mydata.get(getAdapterPosition()).getTarget3().contains("-")) {
+                        msg += " target3@" + mydata.get(getAdapterPosition()).getTarget3() + "";
+                    }
 
+                    Log.d("msg",msg);
+
+
+                    getBitmapFromView(msg);
+                }
+            });
         }
+    }
+
+    public void getBitmapFromView(String txt) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, txt);
+        sendIntent.setType("text/plain");
+        myctx.startActivity(sendIntent);
     }
 
 
